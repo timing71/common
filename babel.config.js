fa18bc619d106d9cd2295cee15b6771c50705919
James Muscat <jamesremuscat@gmail.com>
2022-06-30 18:55:24 +0100

    Leave ES6 modules alone except in test.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   "plugins": ["babel-plugin-transform-import-meta"],
-  "presets": ["@babel/preset-env"]
+  "presets": [ ["@babel/preset-env", { modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false }] ]
 }
