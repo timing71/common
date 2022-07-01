0e33a2aaafd89163f94232fbad4b5ff2fa0a3faf
James Muscat <jamesremuscat@gmail.com>
2022-07-01 21:01:27 +0100

    Do away with Babel all together.
    ES modules might _still_ be somewhat experimental, but it makes the
    build process somewhat more sane (IMO)...

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "plugins": ["babel-plugin-transform-import-meta"],
-  "presets": [ ["@babel/preset-env", { modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false }] ]
-}
