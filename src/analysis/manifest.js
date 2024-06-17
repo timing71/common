951ca0c85d9f9ace48cd5fa472f5bbfe3c1a7df5
James Muscat <jamesremuscat@gmail.com>
2024-06-17 17:08:32 +0100

    Handle both non-millisecond and millisecond timestamps in manifest.

--- src/analysis/manifest.js
+++ src/analysis/manifest.js
@@ -1,8 +1,20 @@
 import { types } from 'mobx-state-tree';
 
-export const Manifest = types.model({
+const ManifestModel = types.model({
   name: types.optional(types.string, ''),
   description: types.optional(types.string, ''),
   colSpec: types.array(types.frozen(types.array)),
-  startTime: types.optional(types.Date, new Date())
+  startTime: types.optional(types.Date, () => new Date())
+});
+
+export const Manifest = types.snapshotProcessor(ManifestModel, {
+  preProcessor(snapshot) {
+    if (!snapshot) return {};
+    if (snapshot.startTime < 1e10) {
+      // Unix-style timestamp without milliseconds; we expect a timestamp
+      // with milliseconds to pass to `new Date()`
+      snapshot.startTime = snapshot.startTime * 1000;
+    }
+    return snapshot;
+  }
 });
